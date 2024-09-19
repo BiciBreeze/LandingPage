a4a03c580b6c409e7dc15210f12e24facb7748ee
Fabricio Apaza Morales <josueapaza01@gmail.com>
2024-09-18 21:18:05 -0500

    feat: added new events for the new section

--- assets/menu.js
+++ assets/menu.js
@@ -8,6 +8,8 @@ function closeNav() {
 
 const home = document.querySelector("#home");
 const about = document.querySelector("#about");
+const plans = document.querySelector("#plans");
+const contact = document.querySelector("#contact");
 
 home.addEventListener("click", (h) => {
     h.preventDefault();
@@ -19,9 +21,21 @@ about.addEventListener("click", (a) => {
     const selectionN = document.querySelector("#about-section");
     selectionN.scrollIntoView({ behavior: "smooth" });
 });
+plans.addEventListener("click", (p) => {
+    p.preventDefault();
+    const selectionN = document.querySelector("#plans-section");
+    selectionN.scrollIntoView({ behavior: "smooth" });
+});
+contact.addEventListener("click", (c) => {
+    c.preventDefault();
+    const selectionN = document.querySelector("#contact-us-section");
+    selectionN.scrollIntoView({ behavior: "smooth" });
+});
 
 const home2 = document.querySelector("#home2");
 const about2 = document.querySelector("#about2");
+const plans2 = document.querySelector("#plans2");
+const contact2 = document.querySelector("#contact2");
 
 home2.addEventListener("click", (h) => {
     h.preventDefault();
@@ -33,6 +47,20 @@ home2.addEventListener("click", (h) => {
 about2.addEventListener("click", (a) => {    
     a.preventDefault();
     const selectionN = document.querySelector("#about-section");
+    selectionN.scrollIntoView({ behavior: "smooth" });
+    closeNav();
+});
+
+plans2.addEventListener("click", (p) => {
+    p.preventDefault();
+    const selectionN = document.querySelector("#plans-section");
+    selectionN.scrollIntoView({ behavior: "smooth" });
+    closeNav();
+});
+
+contact2.addEventListener("click", (c) => {
+    c.preventDefault();
+    const selectionN = document.querySelector("#contact-us-section");
     selectionN.scrollIntoView({ behavior: "smooth" });
     closeNav();
 });
